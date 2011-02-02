cb35172bde80ee4b0d46e25025077dd0281556fd
Ray Toal <rtoal@lmu.edu>
2011-02-02 17:25:57 +0000

    Stub

---
+++ src/main/java/edu/lmu/cs/xlg/roflkode/Compiler.java
@@ -0,0 +1,14 @@
+package edu.lmu.cs.xlg.roflkode;
+
+/**
+ * A Roflkode Compiler.
+ */
+public class Compiler {
+
+    /**
+     * Runs the compiler as an application.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, this is the compiler.");
+    }
+}
