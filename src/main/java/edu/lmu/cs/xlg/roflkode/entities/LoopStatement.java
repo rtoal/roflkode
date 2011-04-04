2f65300aec0bfb5a60c05ae3d692e62cfde0c81a
Ray Toal <rtoal@lmu.edu>
2011-04-04 08:40:02 +0000

    AST generation complete (probably)

--- src/main/java/edu/lmu/cs/xlg/roflkode/entities/LoopStatement.java
+++ src/main/java/edu/lmu/cs/xlg/roflkode/entities/LoopStatement.java
@@ -1,17 +1,67 @@
 package edu.lmu.cs.xlg.roflkode.entities;
 
+import edu.lmu.cs.xlg.util.Log;
+
 /**
- * Abstract superclass of all loop statements.
+ * A Roflkode loop statement.
  */
-public abstract class LoopStatement extends Statement {
+public class LoopStatement extends Statement {
 
-    String name;
+    private String name;
+    private String loopType;
+    private Expression condition;
+    private String iterator;
+    private Expression start;
+    private Expression end;
+    private Expression collection;
+    private Block body;
 
-    public LoopStatement(String name) {
+    public LoopStatement(String name, String loopType, Expression condition, String iterator,
+            Expression start, Expression end, Expression collection, Block body) {
         this.name = name;
+        this.loopType = loopType;
+        this.condition = condition;
+        this.iterator = iterator;
+        this.start = start;
+        this.end = end;
+        this.collection = collection;
+        this.body = body;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getLoopType() {
+        return loopType;
+    }
+
+    public Expression getCondition() {
+        return condition;
+    }
+
+    public String getIterator() {
+        return iterator;
+    }
+
+    public Expression getStart() {
+        return start;
+    }
+
+    public Expression getEnd() {
+        return end;
+    }
+
+    public Expression getCollection() {
+        return collection;
+    }
+
+    public Block getBody() {
+        return body;
+    }
+
+    @Override
+    public void analyze(Log log, SymbolTable table, Function function, boolean inLoop) {
+        // TODO Auto-generated method stub
     }
 }
--- src/main/java/edu/lmu/cs/xlg/roflkode/entities/SwitchStatement.java
+++ src/main/java/edu/lmu/cs/xlg/roflkode/entities/SwitchStatement.java
@@ -5,27 +5,33 @@ import java.util.List;
 import edu.lmu.cs.xlg.util.Log;
 
 /**
- * A Roflkode conditional statement.  Like most languages, it contains a list of one or more arms
+ * A Roflkode switch statement.  Like most languages, it contains a list of one or more arms
  * and an optional else part.
  */
-public class ConditionalStatement extends Statement {
+public class SwitchStatement extends Statement {
 
     public static class Arm {
-        private Expression guard;
+        private Literal guard;
         private Block block;
 
-        public Arm(Expression guard, Block block) {
+        public Arm(Literal guard, Block block) {
             this.guard = guard;
             this.block = block;
         }
     }
 
+    private Expression expression;
     private List<Arm> arms;
     private Block elsePart;
 
-    public ConditionalStatement(List<Arm> arms, Block elsePart) {
+    public SwitchStatement(Expression expression, List<Arm> arms, Block elsePart) {
+        this.expression = expression;
         this.arms = arms;
         this.elsePart = elsePart;
+    }
+
+    public Expression getExpression() {
+        return expression;
     }
 
     public List<Arm> getArms() {
@@ -38,9 +44,10 @@ public class ConditionalStatement extends Statement {
 
     @Override
     public void analyze(Log log, SymbolTable table, Function function, boolean inLoop) {
+        expression.analyze(log, table);
         for (Arm arm: arms) {
             arm.guard.analyze(log, table);
-            arm.guard.assertBoolean("condition", log);
+            // TODO - CHECK TYPE COMPATIBILITY BETWEEN arm.guard.type and expression.type
             arm.block.analyze(log, table, function, inLoop);
         }
         if (elsePart != null) {
